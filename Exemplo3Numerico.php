71f0373ac81f2137e695ed2bb6d107566d66e423
MatheusG <mariaoglanowski@gmail.com>
2025-07-09 22:08:43 -0300

    Funções php

---
+++ Exemplo3Numerico.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $num = 537; //Número Inteiro
+        echo $num, "= Número Inteiro <br/><br/>";
+        $num = -13;
+        echo $num, "= Número Negativo <br/><br/>";
+        $num = 0123;
+        echo $num, "= Número Octal <br/><br/>";
+        $num = 0x1A;
+        echo $num, "= Número Hexadecimal <br/><br/>";
+        $num = 1.234;
+        echo $num, "= Ponto Flutuante <br/><br/>";
+        $num = 4e23;
+        echo $num, "= Notação Cientifica <br/><br/>";
+    ?>
+</body>
+</html>
---
+++ Exemplo4Pizza.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $comida_favorita = "Italiana";
+        print $comida_favorita[2];
+        $comida_favorita = "Cozinha ".$comida_favorita;
+        print $comida_favorita;
+    ?>
+</body>
+</html>
